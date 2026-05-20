d7745fcb00933e9dc0c1ba87f9ff142c4a65b94f
jrcable1 <jrcable1@gmail.com>
2026-05-19 23:52:33 -0400

    add Alpha and device flags

--- alpha_tangorpro.mk
+++ alpha_tangorpro.mk
@@ -27,6 +27,39 @@ PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 2560
 
+# Build config
+TABLET_WIFI_ONLY := true
+
+# append time of day to zip
+ALPHA_VERSION_APPEND_TIME_OF_DAY := false
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_GOOGLE_COMMS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+WITH_PIXEL_OVERLAYS := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Jrcable2
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tangorpro-user 16 BP4A.260205.001 14624666 release-keys" \
     BuildFingerprint=google/tangorpro/tangorpro:16/BP4A.260205.001/14624666:user/release-keys \
